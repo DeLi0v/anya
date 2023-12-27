39958f97812f1cfdb0d7e246408da994296c02cf
DeLi0v <109518046+DeLi0v@users.noreply.github.com>
2023-12-28 01:25:53 +0300

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
 <?php
-$connect = mysqli_connect('192.168.0.3', 'a2', '1', 'consulting') or die ('не работает');
+$connect = mysqli_connect('db', 'root', 'secret', 'delfin') or die ('не работает');
 $connect->query( "SET CHARSET utf8" );
 mysql_set_charset("utf8");
 $connect->set_charset("utf-8");

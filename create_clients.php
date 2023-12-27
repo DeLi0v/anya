4e8159cd9f516f365d6d8efd33c1235b65e2692c
DeLi0v <109518046+DeLi0v@users.noreply.github.com>
2023-12-28 01:40:27 +0300

    Update create_clients.php

--- create_clients.php
+++ create_clients.php
@@ -2,13 +2,12 @@
 
 require_once 'connect.php';
 
-$ID = $_POST['ID'];
 $sname = $_POST['sname'];
 $fname = $_POST['fname'];
 $mname = $_POST['tname'];
 $pnumber = $_POST['pnumber'];
 $basket = $_POST['basket'];
 
-mysqli_query($connect, "INSERT INTO `consulting`.`clients` (`ID clienta`, `SName`, `FName`, `MName`, `PNum`, `Basket`) VALUES ('$ID', '$sname', '$fname', '$mname', '$pnumber', '$basket');");
+mysqli_query($connect, "INSERT INTO `consulting`.`clients` (`SName`, `FName`, `MName`, `PNum`, `Basket`) VALUES ('$sname', '$fname', '$mname', '$pnumber', '$basket');");
 
 header('Location:index.php');
